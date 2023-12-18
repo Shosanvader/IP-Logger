b5b8ba6b091b3a55173f0e319f552bf8bc7e4fa0
Okediadi Praise <okediadip@gmail.com>
2023-12-18 09:11:32 +0100

    Allowed all hosts

--- iplog/settings.py
+++ iplog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)==qgt!epu!t-a%feu340cv9zf_7kp0mzesh$h35j4cdjy(3hh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
