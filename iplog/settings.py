ad136d998796530d5b2d94ae4892d5772e5f17f6
Okediadi Praise <okediadip@gmail.com>
2023-12-18 08:51:55 +0100

    Added secret

--- iplog/settings.py
+++ iplog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)==qgt!epu!t-a%feu340cv9zf_7kp0mzesh$h35j4cdjy(3hh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
